dff83f17c33ed08249dd0b1bc3ff727c4b405189
Hlib Khoruznhyi <gleb5192005@gmail.com>
2023-11-08 13:35:42 +0100

    first commit

---
+++ src/main/java/com/springstudy/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.springstudy.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+
+        return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied(){
+        return "acces-denied";
+    }
+}
---
+++ src/main/java/com/springstudy/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.springstudy.springboot.demosecurity.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // adding users with JDBC
+    @Bean
+    public UserDetailsManager inMemoryUserDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        jdbcUserDetailsManager.setUsersByUsernameQuery("select username, password, activity from users where username=?");
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select username, authorities from authorities where username=?");
+//        return jdbcUserDetailsManager;
+    }
+
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john,mary,susan);
+    }*/
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser").permitAll())
+                .logout(logout -> logout.permitAll())
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied"));
+
+        return http.build();
+    }
+}
